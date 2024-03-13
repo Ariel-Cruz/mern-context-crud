e6655c85cf47778a2fd2a845d9ffc7ddcb3d4bae
Ariel Cruz <acruzo2025@gmail.com>
2024-03-12 21:27:26 -0300

    arreglo config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 
 dotenv.config()
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/postsdb"
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://acruzo2025:acruzo2001@cluster0.lku587s.mongodb.net/cluster0?retryWrites=true&w=majority&appName=Cluster0"
 
 export const PORT = process.env.PORT || 4000
